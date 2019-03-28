eeba9415f125f92da0a68c74ead3cbecd3d23b91
Kaloqn Gochev <volvov401999@abv.bg>
2019-03-28 15:41:37 +0200

    Creating a class Car

---
+++ Cars_Project/Cars_Project/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_Project
+{
+    class Car
+    {
+        
+        
+            public string ID { get; set; }
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public string Year { get; set; }
+            public string Engine { get; set; }
+            public string Fuel { get; set; }
+            public string Doors { get; set; }
+            public string Condition { get; set; }
+            public string Price { get; set; }
+        }
+    }
+
